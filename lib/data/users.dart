5e303286bcd67038d45b715883018d4be02f7d57
Khin Me Me Latt <khinmemelatt@outlook.com>
2021-05-29 15:15:18 +0630

    Initial commit'

---
+++ lib/data/users.dart
@@ -0,0 +1,25 @@
+import 'package:flutterstoryapp/data/stories.dart';
+import 'package:flutterstoryapp/model/user.dart';
+
+final users = [
+  User(
+    name: 'Gary',
+    imgUrl: 'assets/user2.png',
+    stories: stories,
+  ),
+  User(
+    name: 'Michael',
+    imgUrl: 'assets/user3.png',
+    stories: stories2,
+  ),
+  User(
+    name: 'John',
+    imgUrl: 'assets/user1.png',
+    stories: stories,
+  ),
+  User(
+    name: 'Emma',
+    imgUrl: 'assets/user4.png',
+    stories: stories2,
+  ),
+];
